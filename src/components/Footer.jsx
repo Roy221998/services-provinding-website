6c0118eb3976744890e3310bd20bfaa3e129a5ad
Debabrata Roy <77378716+Roy221998@users.noreply.github.com>
2023-01-11 09:44:02 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow us</h5>
+        <div>
+          <a href="https://youtube/.com/debabrataroy" target={"blank"}>Youtube</a>
+        </div>
+      </div>
+    </footer>
+  
+}
+
+export default Footer;
